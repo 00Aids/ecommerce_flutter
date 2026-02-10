fd4b9916e17f327dd6df2ffcfb130322c635e1d1
00Aids <johanarley352@gmail.com>
2026-02-09 21:59:07 -0500

    Primer commit e inicializacion de proyecto

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:ecommerce_fluter/src/presentation/pages/auth/login/LoginPage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      initialRoute: 'login',
+      routes: {
+        'login': (BuildContext context) => Loginpage()
+      },
+    );
+  }
+}
